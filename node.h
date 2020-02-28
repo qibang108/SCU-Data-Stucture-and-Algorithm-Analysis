33076cf9a17239b531532c27e958afdb89c9b18d
qibang108 <53903949+qibang108@users.noreply.github.com>
2020-02-28 16:54:09 +0800

    Create node.h

---
+++ node.h
@@ -0,0 +1,18 @@
+//Class of Node in linkArray
+
+template <class T>class Link {                          
+public:
+	T element;                //element of a node
+	Link*next;                //pointer to next
+	
+    Link(const T& elemval, Link* nextval = NULL)
+	{
+		element = elemval; next = nextval;
+	}                       
+    
+    // set up the tail node
+    Link(Link* nextval = NULL) {
+		next = nextval;
+	}                      
+};
+
