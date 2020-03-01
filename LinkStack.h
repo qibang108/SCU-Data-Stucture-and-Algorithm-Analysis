82e4f53ae454459d6be3c85ae826e663ddbc9c50
qibang108 <53903949+qibang108@users.noreply.github.com>
2020-03-01 16:40:05 +0800

    Update LinkStack.h

--- LinkStack.h
+++ LinkStack.h
@@ -1,17 +1,7 @@
 #pragma once
 #include "node.h"
-template <class T>class Link {                               
-public:
-	T element;     //结点处的值
-	Link*next;     //指向下一结点处的指针
-	Link(const T& elemval, Link* nextval = NULL)  //拷贝构造函数
-	{
-		element = elemval; next = nextval;
-	}
-	Link(Link* nextval = NULL) { next = nextval; }  //设置尾结点
-};
-template <class Elem>
-class LinkStack{
+
+template <class Elem>class LinkStack{
 private:
 	Link<Elem>* top;
 	int size;
@@ -25,4 +15,3 @@ public:
 	int length() const { return top; }
 	void printStack();
 };
-
